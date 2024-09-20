0a501d1becc962e8991abc3d620e7dde8e565234
Chizhakula <slava.chizh00@gmail.com>
2024-09-20 18:07:18 +1000

    Add files via upload

---
+++ js практическая/script.js
@@ -0,0 +1,28 @@
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// let resault = (a+b)*c;
+// console.log(resault)
+
+// let num = [2,9.4,18.1,21,46.3];
+// let sum = 0;
+// for(let i = 0; i < num.length; i++){
+//     sum += num[i]  ;
+// }
+// console.log(sum)
+
+// function convert(rub){
+//     let cours = 13
+//     const yan = rub * cours
+//     return yan;
+// }
+// const rub = 250;
+// console.log(rub, "RUR" ,"->" ,convert(rub), "CNY")
+
+// const metr = (sm) => {
+//     return sm/100
+// }
+// const sm = 150
+// console.log(metr(sm))
+
